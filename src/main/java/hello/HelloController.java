a95b5d7abd8784e8502222d6262becbee62a443e
mabvelho <mabvelho@gmail.com>
2018-08-23 16:46:37 -0300

    Joda time added #3

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -2,13 +2,19 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.joda.time.LocalTime;
+import org.joda.time.LocalDate;
+
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping("/data")
     public String index() {
-        return "Greetings from Spring Boot!";
+		LocalTime currentTime = new LocalTime();
+		LocalDate currentDate = new LocalDate();
+		
+        return "Greetings from Spring Boot!" + currentDate + " " + currentTime;
     }
 
 }
